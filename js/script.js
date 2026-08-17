9d1ba98cc0ec1543b20872210e0910573e1dc43c
unknown <poshalibanerjee3@gmail.com>
2026-08-17 22:45:32 +0530

    Remove node_modules from repository

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-let toggler=document.getElementById('toggler');
-toggler.addEventListener('click',toggling);
+let toggler = document.getElementById("toggler");
+let mobileMenu = document.getElementById("mobileMenu");
 
-function toggling()
-{
-    // alert("Clicked");
-    document.querySelector('navlist').classList.toggle('.showNavlist')
-}
+toggler.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+});
+
+const testimonialSwiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    navigation: {
+      nextEl: ".testimonial-next",
+      prevEl: ".testimonial-prev",
+    },
+
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
